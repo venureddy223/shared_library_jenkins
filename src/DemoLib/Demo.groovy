83687cab2512bf98275129884faa790e2236a60b
venureddy223 <thipparthyvenugopalreddy@gmail.com>
2024-05-20 16:28:27 -0700

    fix demo

--- src/DemoLib/Demo.groovy
+++ src/DemoLib/Demo.groovy
@@ -3,13 +3,11 @@ package DemoLib
 
 class Demo{
     String name
-
     Demo(String name){
         this.name = name
     }
 
     def speak(){
-        return "${name} says bow!"
+        return "${name} says hey"
     }
-
 }
