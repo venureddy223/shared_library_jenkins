759ffc8c9bc0be1f6a0e383349000ec6d07a4bc1
venureddy223 <thipparthyvenugopalreddy@gmail.com>
2024-05-20 21:09:00 -0700

    adding randomnumbergenerator add def

--- src/DemoLib/randomNumberGenerator.groovy
+++ src/DemoLib/randomNumberGenerator.groovy
@@ -4,7 +4,7 @@ package DemoLib
 import java.util.concurrent.ThreadLocalRandom
 
 class randomNumberGenerator{
-    def randomNumberGenerator(min, max) {
+    int randomNumberGenerator(min, max) {
         return ThreadLocalRandom.current().nextInt(min,max+1)
     }
 }
